43887c398cedb1a083435f8bf416cedb1b2eae98
Jone <joonas.laakkonen@gmail.com>
2014-03-13 14:34:55 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cabinmaster::Application.config.secret_key_base = '419d26fceb4c2663732de9cbc49dd530b83d896de858167e37b99682e747e6e341c805c133b2eb3568420f4edd08a0e0d9f39322d9a2dffae0de259a2f9b7ec2'
