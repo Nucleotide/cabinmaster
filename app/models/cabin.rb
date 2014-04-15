c1c00c52d616ea61623ac33f6a4756f3756ee0fd
Jone <joonas.laakkonen@gmail.com>
2014-04-15 13:50:26 +0300

    to heroku from travis

--- app/models/cabin.rb
+++ app/models/cabin.rb
@@ -5,9 +5,13 @@ class Cabin < ActiveRecord::Base
   has_many :users, :through => :visits
   has_many :visits, :dependent => :destroy
 
-  def all_visits
+  def next_visit
+    if Visit.first.nil?
+      return nil
+    else
     visit = Visit.first
     return "Start date " + visit.start_date.to_s + " End date " + visit.end_date.to_s + " Visitor " + visit.user_name(visit.user_id)
+    end
   end
 
 end
